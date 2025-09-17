f5ce9ffec77f806d353b3ece3ca197abfb3bf71f
Oleksandra Gavrylenko <aleksgavrik25@gmail.com>
2025-09-17 08:57:34 -0400

    lesson 01

---
+++ Lesson01-conditionals/python.py
@@ -0,0 +1,48 @@
+# ========================================
+# ACCELERATED PYTHON UNIT 2 - LESSON 1
+# Conditionals: JavaScript to Python
+# ========================================
+
+# ========================================
+# SECTION 1: QUICK TRANSLATION CHALLENGE
+# ========================================
+print("==Grade Classification==")
+score = 87
+if score >= 90:
+    print("A grade!")
+elif score >= 80:
+    print("B grade")
+else:
+    print('Failed')
+
+# ========================================
+# SECTION 2: AGE CATEGORY CLASSIFIER
+# ========================================
+age_input = input('Enter ur age: ')
+if age_input:
+    age = int(age_input)
+    if 0<=age<=12:
+        print('ur a child')
+    elif 13<=age<=19:
+        print('ur a teeni teen')
+    elif 20<=age<=64:
+        print('wowww soooo mature')
+    elif age>=65:
+        print('so old')
+    else:
+        print('error error enter proper age')
+else:
+    print('bruh. invalid input.')
+# ========================================
+# SECTION 3: STUDENT STATUS CHECKER
+# ========================================
+
+
+# ========================================
+# SECTION 4: GRADE VALIDATOR CHALLENGE
+# ========================================
+
+
+# ========================================
+# SECTION 5: WEATHER DECISION SYSTEM
+# ========================================
