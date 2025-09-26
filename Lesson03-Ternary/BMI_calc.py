90f0fe02e6b8eae6bf12ec1ddb96fa0780aa73f1
Oleksandra Gavrylenko <aleksgavrik25@gmail.com>
2025-09-25 22:30:25 -0400

    finished BMI calc

--- Lesson03-Ternary/BMI_calc.py
+++ Lesson03-Ternary/BMI_calc.py
@@ -1,11 +1,31 @@
-entered_height = int(input('enter height (in): '))
-entered_weight = int(input('enter weight (lb): '))
+print('BMI calc')
+entered_height = float(input('enter height (in): '))
+entered_weight = float(input('enter weight (lb): '))
 height = entered_height if entered_height >= 0 else print('enter proper value')
 weight = entered_weight if entered_weight >= 0 else print('enter proper value')
-BMI = (weight/(height^2))*703
+BMI = (weight/(height*height))*703
 status = ('Underweight' if BMI < 18.5 else
           "Normal" if 18.5<=BMI<25 else
           "Overweight" if 25<=BMI<30 else
           "Obese" if BMI >= 30 else
           'error')
-print(status)
+print('')
+print('=====================')
+print('BMI Health Report')
+print('=====================')
+print(f'height: {height}"')
+print(f'weight: {weight}')
+print(f'BMI: {BMI}')
+print(f'category: {status}')
+if status == 'Underweight':
+    print('Recomendation: Gain Weight')
+    print('Health Risk: Medium')
+elif status == 'Normal':
+    print('Recomendation: Maintain Weight')
+    print('Health Risk: Low')
+elif status == 'Overweight':
+    print('Recomendation: Lose Weight')
+    print('Health Risk: Medium')
+elif status == 'Obese':
+    print('Recomendation: Lose a lot of Weight')
+    print('Health Risk: High')
