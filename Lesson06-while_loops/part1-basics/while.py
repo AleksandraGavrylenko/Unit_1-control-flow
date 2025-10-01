d2463bf9f6196eebda44b484ac3d13b666740eb6
Oleksandra Gavrylenko <aleksgavrik25@gmail.com>
2025-10-01 08:55:39 -0400

    lesson 6 part 1

---
+++ Lesson06-while_loops/part1-basics/while.py
@@ -0,0 +1,39 @@
+#syntax
+'''
+initialize variable
+while condition:
+    code block
+    use variable (+ / -)
+'''
+num = 5
+while num >=1:
+    print(num)
+    num -=1
+print('blast off') 
+
+sum = 0
+num = 0
+# while num <=10:
+#     sum += num 
+#     num += 1
+# print(sum) 
+while num <=10:
+    sum+=num 
+    if num < 10:
+        print(num, end=' + ')
+    else:
+        print(num, end=' = ')
+    num +=1
+print(sum)
+# sum of digits: take user input as int, and find the sum of it's digits. 
+number = '1234' #input('enter a number')
+sum = 0
+for char in number:
+    #print(f'{char} {type(char)}')
+    sum += int(char)
+print(sum)
+
+i=0
+while i<len(number):
+    sum+= int(number[i])
+    i +=1
