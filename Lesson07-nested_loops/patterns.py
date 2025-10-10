5bb09bb3d349fc53c5862a173f3bb955a6990e29
Oleksandra Gavrylenko <aleksgavrik25@gmail.com>
2025-10-10 08:38:17 -0400

    dfghjk

--- Lesson07-nested_loops/patterns.py
+++ Lesson07-nested_loops/patterns.py
@@ -1,5 +1,13 @@
 #pyramid pattern
 rows = 5
 n = 1
-for i in range(1,rows+1):
-    print(f'{' '*(5-rows)}{'*'*(1+2*(rows-1))}')
+#step 1 create outer loop for rows
+for i in range(rows):
+    #step 2 print the spaces rows - i -1
+    for j in range(rows-i-1):
+        print(' ', end='')
+    #print the stars 2*i + 1
+    for k in range(2*i+1):
+        print('*', end='')
+    #step 4 print new line 
+    print()
